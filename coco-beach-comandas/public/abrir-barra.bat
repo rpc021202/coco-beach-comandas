b6983f54f97a8636a838677201f19441e7b02412
rpc021202 <rpc021202@gmail.com>
2026-07-23 14:20:41 +0200

    Create abrir-barra.bat

---
+++ coco-beach-comandas/public/abrir-barra.bat
@@ -0,0 +1,3 @@
+@echo off
+title Coco Beach - Barra
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk-printing --start-maximized "https://coco-beach-comandas.onrender.com/barra.html"
