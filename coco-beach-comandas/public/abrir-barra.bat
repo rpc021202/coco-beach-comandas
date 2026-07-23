7f82daa366f85e137be52c9bb1df9b4bea856a18
rpc021202 <rpc021202@gmail.com>
2026-07-23 16:41:29 +0200

    Update abrir-barra.bat

--- coco-beach-comandas/public/abrir-barra.bat
+++ coco-beach-comandas/public/abrir-barra.bat
@@ -1,3 +1,7 @@
 @echo off
 title Coco Beach - Barra
+echo Cerrando Chrome...
+taskkill /F /IM chrome.exe /T 2>nul
+timeout /t 2 /nobreak >nul
+echo Abriendo Coco Beach Barra...
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk-printing --start-maximized "https://coco-beach-comandas.onrender.com/barra.html"
